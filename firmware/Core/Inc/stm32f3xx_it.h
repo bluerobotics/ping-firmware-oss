7dfa86c648ea094c80874e7dd10bb0d3dd080c0f
Joao Mario Lago <joao.mario.lago@hotmail.com>
2025-01-20 04:37:55 -0300

    firmware: Add ADC4 (Piezo Echo Reading) config

--- firmware/Core/Inc/stm32f3xx_it.h
+++ firmware/Core/Inc/stm32f3xx_it.h
@@ -60,6 +60,7 @@ void DMA1_Channel5_IRQHandler(void);
 void TIM1_UP_TIM16_IRQHandler(void);
 void TIM2_IRQHandler(void);
 void USART1_IRQHandler(void);
+void DMA2_Channel2_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
